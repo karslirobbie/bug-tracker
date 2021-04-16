20642ed56949dbffc4d07630b00d335983fb2c5f
Robbie Karsli <clementerobbie@gmail.com>
2021-04-16 21:20:32 +0800

    Created Description List

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Nav from './common/Nav'
 import Header from './common/Header'
 import Table from './common/Table'
-
+import DescriptionList from './common/DescriptionList'
 
 export default function Ticket () {
   return (
-    <div className="flex overflow-hidden w-full h-full text-xs text-gray-3000 bg-gray-800">
+    <div className="flex place-items-center overflow-hidden w-full h-full text-xs text-gray-3000 bg-gray-800">
       <Nav />
 
       <div className="flex flex-col xl:w-1/2 w-full h-full">
@@ -16,8 +16,8 @@ export default function Ticket () {
         </main>
       </div>
 
-      <div className="ticket-description">
-
+      <div className="hidden xl:flex xl:w-1/2 h-3/4">
+        <DescriptionList />
       </div>
     </div>
   )
